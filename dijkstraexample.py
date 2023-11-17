7f1ea6a05f8c00d0c235914dbb9ac29a28d232ff
Cole <132639101+coledombrowski@users.noreply.github.com>
2023-11-16 22:05:30 -0500

    Add files via upload

---
+++ dijkstraexample.py
@@ -0,0 +1,33 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    pq = [(0, start)]
+
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
+
+# Example usage
+graph = 
+{
+    'A': {'B': 3, 'C': 1},
+    'B': {'A': 3, 'C': 7, 'D': 5},
+    'C': {'A': 1, 'B': 7, 'D': 2},
+    'D': {'B': 5, 'C': 2}
+}
+
+start_node = 'A'
+shortest_distances = dijkstra(graph, start_node)
+print("Shortest distances from node", start_node, "to other nodes:", shortest_distances)
