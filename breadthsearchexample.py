53eed9206038cc3221fc5a34a1622703a96a3376
Cole <132639101+coledombrowski@users.noreply.github.com>
2023-11-16 21:59:32 -0500

    Add files via upload

---
+++ breadthsearchexample.py
@@ -0,0 +1,19 @@
+# Iterative implementation of BFS using a queue
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Example
+bfs(graph, 'A')
---
+++ depthfirstsearchexample.py
@@ -0,0 +1,23 @@
+# Recursive implementation of DFS
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Example
+graph = 
+{
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+dfs(graph, 'A')
